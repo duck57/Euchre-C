74e1fce4deeca7c0e5b4a4233363d153c13b1455
Chris Matlak <chrismatlak@Chris-Matlaks-MacBook-Pro.local>
2014-03-28 01:01:41 -0500

    Initial Commit

---
+++ Euchre/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  Euchre
+//
+//  Created by Chris Matlak on 3/28/14.
+//  Copyright (c) 2014 TimmyJ_NET. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
